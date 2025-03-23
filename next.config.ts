5f7b0419d008596ac665e6ef465895d0621280fe
jd2262 <jd2262@cornell.edu>
2025-03-23 00:48:40 -0400

    Updated next config file

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  swcMinify: true,
+  output: 'standalone',
 };
 
 export default nextConfig;
