ee456fa2d0b03871dd1d1e2fef268fcaa8c6db02
jd2262 <jd2262@cornell.edu>
2025-03-23 00:38:05 -0400

    Added website title and icon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,16 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "../contexts/ThemeContext";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider>
+      <Head>
+        <title>Joshua Dirga</title>
+        <link rel="icon" href="/images/joshua-headshot-round.png" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
       <Component {...pageProps} />
     </ThemeProvider>
   );
