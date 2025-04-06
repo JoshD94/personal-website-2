3a0c267e301ec050decd5188c0023e96c7da65e0
Joshua Dirga <joshdirga@gmail.com>
2025-04-06 14:14:09 -0400

    Added analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,12 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "../contexts/ThemeContext";
 import Head from "next/head";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider>
+      <Analytics />
       <Head>
         <title>Joshua Dirga</title>
         <link rel="icon" href="/images/joshua-headshot-round.png" />
