8cc60b767727d1450c47191e9f2c5d896e7e0626
Joshua Dirga <joshdirga@gmail.com>
2025-05-03 19:48:53 -0400

    Changed linkedin url

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,4 @@
 // Social links
 export const GITHUB_URL = process.env.NEXT_PUBLIC_GITHUB_URL || "https://github.com/JoshD94";
-export const LINKEDIN_URL = process.env.NEXT_PUBLIC_LINKEDIN_URL || "https://www.linkedin.com/in/joshua-dirga-49a929215/";
+export const LINKEDIN_URL = process.env.NEXT_PUBLIC_LINKEDIN_URL || "https://www.linkedin.com/in/joshua-dirga/";
 export const RESUME_URL = process.env.NEXT_PUBLIC_RESUME_URL || "https://drive.google.com/file/d/1rURpeqlz-s0BHR177Ffj5PwhDxAmUh1W/view?usp=sharing";
